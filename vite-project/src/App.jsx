cb6930e1aec6638a8bc5a543630a6c1486289b8b
Rei-sama077 <149963916+Rei-sama077@users.noreply.github.com>
2025-07-02 10:47:32 +0800

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "./Sections/Navbar";
+import Hero from "./Sections/Hero";
+import About from "./Sections/About";
+
+
+const App = () => {
+  return (
+    <div classname = "container mx-auto max-w-7xl">
+    <Navbar />
+    <Hero />
+    <About />
+    <section className= "min-h-screen"  />
+      <section className= "min-h-screen"  />
+      <section className= "min-h-screen"  />
+      <section className= "min-h-screen"  />
+      <section className= "min-h-screen"  />
+      <section className= "min-h-screen"  />
+    {/* about */}
+    {/* projects */}
+    {/* experience */}
+    {/* testimonial */}
+    {/* contact */}
+    {/* footer */}
+    </div>
+  );
+};
+
+export default App
---
+++ vite-project/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+import {Html, useProgress} from "@react-three/drei"; 
+const Loader = () => {
+    const {progress} = useProgress();
+  return <Html center className="text-2xl font-black text-center">{progress}% Loaded</Html>
+};
+
+export default Loader
