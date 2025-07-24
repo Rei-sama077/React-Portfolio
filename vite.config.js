4967562c6fdd2754470e65b3fc3bbc180b27291e
Rei-sama077 <149963916+Rei-sama077@users.noreply.github.com>
2025-07-24 19:18:21 +0800

    Updated

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-Portfolio/',
+  base: isGitHubPages ? '/React-Portfolio/' : '/',
   plugins: [react(), tailwindcss(),],
 });
