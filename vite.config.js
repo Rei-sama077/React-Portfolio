b4f1a0ea54c6e639b2f74b1803ea188bea4b3a79
Rei-sama077 <149963916+Rei-sama077@users.noreply.github.com>
2025-07-25 12:11:48 +0800

    changed base path

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-base: '/React-Portfolio/',
+base: '/',
 plugins: [react(), tailwindcss(),],
 });
