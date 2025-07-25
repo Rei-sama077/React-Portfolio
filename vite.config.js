e34a8de123599411e00a5994ca60d6724ed96a04
Rei-sama077 <149963916+Rei-sama077@users.noreply.github.com>
2025-07-25 12:01:16 +0800

    Added sass

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES';
+// const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: isGitHubPages ? '/React-Portfolio/' : '/',
-  plugins: [react(), tailwindcss(),],
+base: '/React-Portfolio/',
+plugins: [react(), tailwindcss(),],
 });
